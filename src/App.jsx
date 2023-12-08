2d51260958193fc4d4a8f9fb4c94cbda378a8e5c
tanzir ibne ali <tanziribneali@gmail.com>
2023-12-08 22:46:14 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home/Home"
+import About from './pages/About/About'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
         </Routes>
       </Router>
     </>
