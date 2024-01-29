a58bf347ca04f42c1df40b2af2a5309f4a211ad8
tanzir ibne ali <tanziribneali@gmail.com>
2024-01-29 09:51:36 +0600

    link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,63 @@
-
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo/logo.png";
-import "../../styles/HeaderStyle.css"
+import "../../styles/HeaderStyle.css";
 import { useState } from "react";
 
-
 function Header() {
-    const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
-    // scroll Navber
+  // scroll Navber
 
-    const changeValueOnScroll = () => {
-        const scrollValue = document?.documentElement?.scrollTop;
+  const changeValueOnScroll = () => {
+    const scrollValue = document?.documentElement?.scrollTop;
 
-        scrollValue > 100 ? setNav(true) : setNav(false)
-    }
+    scrollValue > 100 ? setNav(true) : setNav(false);
+  };
 
-    window.addEventListener("scroll" , changeValueOnScroll)
+  window.addEventListener("scroll", changeValueOnScroll);
 
   return (
     <header>
-      <Navbar collapseOnSelect expand="lg" className={ ` ${nav === true ? "sticky" : ""}`  } >
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className={` ${nav === true ? "sticky" : ""}`}
+      >
         <Container>
           <Navbar.Brand href="#home">
-          <Link to="/" className="logo">
-            <img src={Logo} alt="Logo" className="img-fluid"/>
-          </Link>
+            <Link to="/" className="logo">
+              <img src={Logo} alt="Logo" className="img-fluid" />
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About</Nav.Link>
-            <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
-            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
-            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/"><div className="cart">
-            <i className="bi bi-bag fs-5"></i>
-            <em className="roundpoint">2</em>
-            </div></Nav.Link>
-          </Nav>
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Our Menu
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Shop
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                Contact
+              </Nav.Link>
+              <Nav.Link as={Link} to="/">
+                <div className="cart">
+                  <i className="bi bi-bag fs-5"></i>
+                  <em className="roundpoint">2</em>
+                </div>
+              </Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
