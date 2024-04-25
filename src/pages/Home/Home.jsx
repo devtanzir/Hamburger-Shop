66b83e68b2b8cc7033a8da72ee216f80fd99bfb7
tanzir ibne ali <tanziribneali@gmail.com>
2024-04-25 17:28:43 +0600

    done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import Layout from '../../components/Layouts/Layout';
-import Section1 from './section1';
-import '../../styles/Home.css'
-import Section2 from './section2';
-import Section3 from './Section3';
-import Section4 from './Section4';
-import Section5 from './section5';
-import Section6 from './Section6';
-import Section7 from './Section7';
+import React from "react";
+import Layout from "../../components/Layouts/Layout";
+import Section1 from "./section1";
+import "../../styles/Home.css";
+import Section2 from "./Section2.jsx";
+import Section3 from "./Section3.jsx";
+import Section4 from "./Section4.jsx";
+import Section5 from "./section5.jsx";
+import Section6 from "./Section6.jsx";
+import Section7 from "./Section7.jsx";
 
 function Home() {
-    return (
-        <div>
-            <Layout>
-                <Section1/>
-                <Section2/>
-                <Section3/>
-                <Section4/>
-                <Section5/>
-                <Section6/>
-                <Section7/>
-            </Layout>
-        </div>
-    );
+  return (
+    <div>
+      <Layout>
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
+        <Section7 />
+      </Layout>
+    </div>
+  );
 }
 
 export default Home;
