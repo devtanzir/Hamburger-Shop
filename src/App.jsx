74ed64a4126c2a7daedd316ab942bb0ceaf9db67
tanzir ibne ali <tanziribneali@gmail.com>
2024-04-25 17:03:02 +0600

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from "./pages/Home/Home"
-import About from './pages/About/About'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
